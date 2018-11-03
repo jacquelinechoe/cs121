ed84f4ec238783456de80c2b2c5fdae31dbde316
jchoe <jchoe20@cmc.edu>
2018-11-02 20:34:38 -0700

    errors/debugging

--- Desktop/ios/UserLoginAndRegistration/UserLoginAndRegistration/ViewController.swift
+++ Desktop/ios/UserLoginAndRegistration/UserLoginAndRegistration/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
         }
         self.performSegue(withIdentifier: "loginView", sender: self);
     }
-
+    
     
     @IBAction func logoutButtonTapped(_ sender: Any) {
         UserDefaults.standard.set(false, forKey:"isUserLoggedIn");
@@ -40,4 +40,3 @@ class ViewController: UIViewController {
     }
     
 }
-
