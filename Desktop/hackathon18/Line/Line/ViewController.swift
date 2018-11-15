767b6d9594c1832d1ed30572f9fb905df00b64aa
jchoe <jchoe20@cmc.edu>
2018-11-15 13:14:17 -0800

    hackathon

---
+++ Desktop/hackathon18/Line/Line/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Line
+//
+//  Created by Jacqueline Choe on 11/2/18.
+//  Copyright © 2018 twentyEights. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
